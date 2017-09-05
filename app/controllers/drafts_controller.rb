f7760954855c72b44429a54c2f62fbeb1f4a24a8
Krishna Kulkarni <krishna.kulkarni19@gmail.com>
2017-09-05 03:10:17 -0400

    Add drafting from draft show page

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -1,10 +1,20 @@
 class DraftsController < ApplicationController
-  before_action :set_league, only: [:show]
+  before_action :set_league, only: [:show, :update]
 
-  # GET /leagues/1/draft
   # GET /leagues/1/draft
   def show
     @scarcity_data = ScarcityIndicator.run(@league)
+  end
+
+  # PATCH /leagues/1/draft
+  def update
+    roster = Roster.find(params[:roster_id])
+    player = Player.find(params[:player_id])
+    cost = params.fetch(:cost).to_i
+
+    roster.draft(player, cost)
+
+    redirect_to league_draft_path(@league), notice: "#{roster.name} drafted #{player.name} for $#{cost}"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :rosters
   resources :managers
   resources :leagues do
-    resource :draft, only: [:show]
+    resource :draft, only: [:show, :update]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
